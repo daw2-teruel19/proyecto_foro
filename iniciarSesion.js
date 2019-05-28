34ea221ff515d03901d3cfb1153078147a70961b
daw2-teruel19 <43414988+daw2-teruel19@users.noreply.github.com>
2019-05-28 18:01:54 +0200

    subida1

---
+++ iniciarSesion.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+});
+
+
+$("#iniciarSesion").click(function(){
+window.open("Iniciar_Sesion.html", "", "width=220,height=280");
+});
